a2326c3b194d98ac69d1387ac71d32983d8288c7
shijinge <ex-shijinge.xianglin.cn>
2018-09-14 18:07:37 +0800

    提交

--- server/routes/connect.js
+++ server/routes/connect.js
@@ -2,22 +2,23 @@
  * Created by ex-wangxin on 2018/9/13.
  */
 var mysql = require("mysql");
-var pool = mysql.createConnection({
-    host     : 'localhost',
+/*var pool = mysql.createConnection({
+    host     : '172.16.15.205',
     port: '3306',
     user     : 'root',
     password : '123456',
     database: 'wx',
     typeCast: true,             // 是否把结果值转换为原生的 javascript 类型
-});
-/*var pool = mysql.createConnection({
+});*/
+var pool = mysql.createConnection({
     host     : '172.16.12.145',
     port: '3306',
-    user     : 'fuyf',
-    password : '961010',
+    user     : 'root',
+    password : '123456',
     database: 'test',
     typeCast: true,             // 是否把结果值转换为原生的 javascript 类型
-});*/
+});
+
 
 pool.connect(function(err,connection){
     if(err){
@@ -29,7 +30,9 @@ pool.connect(function(err,connection){
     }
 });
 function query(sql,callback){
+    console.log(1111111);
     pool.query(sql, function (err,rows) {
+        console.log(2222222222);
         callback(err,rows);
         //pool.end();
     });
