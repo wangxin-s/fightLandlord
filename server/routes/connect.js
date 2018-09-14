bdcc0e83b6cd848f00f1869b59a1a6b4dca276c3
lijinwen <ex-lijinwen@xianglin.cn>
2018-09-14 18:04:44 +0800

    斗地主

--- server/routes/connect.js
+++ server/routes/connect.js
@@ -2,28 +2,28 @@
  * Created by ex-wangxin on 2018/9/13.
  */
 var mysql = require("mysql");
+// var pool = mysql.createConnection({
+//     host     : 'localhost',
+//     port: '3306',
+//     user     : 'root',
+//     password : '123456',
+//     database: 'wx',
+//     typeCast: true             // 是否把结果值转换为原生的 javascript 类型
+// });
 var pool = mysql.createConnection({
     host     : 'localhost',
     port: '3306',
     user     : 'root',
-    password : '123456',
-    database: 'wx',
-    typeCast: true,             // 是否把结果值转换为原生的 javascript 类型
-});
-/*var pool = mysql.createConnection({
-    host     : '172.16.12.145',
-    port: '3306',
-    user     : 'fuyf',
-    password : '961010',
+    password : '123',
     database: 'test',
     typeCast: true,             // 是否把结果值转换为原生的 javascript 类型
-});*/
+});
 
 pool.connect(function(err,connection){
     if(err){
         console.log("链接失败");
         console.log(err);
-        throw(err)
+        // throw(err)
     }else{
         console.log("链接成功");
     }
