3b6d8449e753e90bf36117e988831b9ab5d71d24
chenwentao <ex-chenwentao@xianglin.cn>
2018-09-17 10:01:36 +0800

    q

--- server/routes/connect.js
+++ server/routes/connect.js
@@ -2,32 +2,7 @@
  * Created by ex-wangxin on 2018/9/13.
  */
 var mysql = require("mysql");
-<<<<<<< HEAD
-// var pool = mysql.createConnection({
-//     host     : 'localhost',
-//     port: '3306',
-//     user     : 'root',
-//     password : '123456',
-//     database: 'wx',
-//     typeCast: true             // 是否把结果值转换为原生的 javascript 类型
-// });
-// var pool = mysql.createConnection({
-//     host     : 'localhost',
-//     port: '3306',
-//     user     : 'root',
-//     password : '123',
-//     database: 'test',
-//     typeCast: true,             // 是否把结果值转换为原生的 javascript 类型
-// });
-=======
-/*var pool = mysql.createConnection({
-    host     : '172.16.15.205',
-    port: '3306',
-    user     : 'root',
-    password : '123456',
-    database: 'wx',
-    typeCast: true,             // 是否把结果值转换为原生的 javascript 类型
-});*/
+
 var pool = mysql.createConnection({
     host     : '172.16.12.145',
     port: '3306',
@@ -37,7 +12,7 @@ var pool = mysql.createConnection({
     typeCast: true,             // 是否把结果值转换为原生的 javascript 类型
 });
 
->>>>>>> 64f31a376507990fa704323407e3c0c29f871ee4
+
 
 pool.connect(function(err,connection){
     if(err){
@@ -51,9 +26,7 @@ pool.connect(function(err,connection){
     }
 });
 function query(sql,callback){
-    console.log(1111111);
     pool.query(sql, function (err,rows) {
-        console.log(2222222222);
         callback(err,rows);
         //pool.end();
     });
